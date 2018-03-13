53c1862a51f618ee2ca33727de7bc3c2ce6d8b7a
viktoriyadi <vikstoyyy@abv.bg>
2018-03-13 17:03:12 +0200

    Added exercises.

---
+++ some exercises/Drawing a romb.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int middleDashes = 0;
+
+            if (n==1)
+            {
+                Console.WriteLine("*");
+                return;
+            }
+            
+            if (n%2!=0)
+            {
+                middleDashes++;
+                Console.WriteLine(new string('-', n / 2) + "*" + new string('-', n / 2));
+            }
+
+            for (int i = 0; i < n/2; i++)
+            {
+                Console.Write(new string('-', n / 2 - 1 - i) + "*");
+                Console.Write(new string('-',middleDashes+ 2 * i) + "*");
+                Console.WriteLine(new string('-', n / 2 - 1-i));
+            }
+            for (int i = n/2-2; i >=0; i--)
+            {
+                Console.Write(new string('-', n / 2 - 1 - i) + "*");
+                Console.Write(new string('-', middleDashes+2 * i) + "*");
+                Console.WriteLine(new string('-', n / 2 - 1 - i));
+            }
+            if (n % 2 != 0)
+            {
+                Console.WriteLine(new string('-', n / 2) + "*" + new string('-', n / 2));
+            }
+        }
+    }
+}
